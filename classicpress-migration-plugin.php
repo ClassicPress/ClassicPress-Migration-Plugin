781f989620da0c994300a2460999185f246ce010
Marc Tenax <42794352+marctenax@users.noreply.github.com>
2018-08-30 12:57:49 +0200

    Initial commit

--- classicpress-migration-plugin.php
+++ classicpress-migration-plugin.php
@@ -1,29 +1,29 @@
 <?php 
 /*
-Plugin Name:       ClassicPress Migration Plugin
-Plugin URI:        https://github.com/classicpress/classicpress-migration-plugin/
+Plugin Name:       Switch to ClassicPress from WordPress
+Plugin URI:        https://github.com/classicpress/switch-to-classicpress-from-wordpress/
 Description:       ClassicPress migration plugin, and say Goodbye to Gutenberg.
-Version:           1.0.1
+Version:           1.0.2
 Requires at least: 4.9
 Tested up to:      4.9
 Requires PHP:      5.2.4
-Author:            ClassicPress Contributors
+Author:            ClassicPress
 Author URI:        https://classicpress.net/
 License:           GPLv2 or later
 License URI:       https://www.gnu.org/licenses/gpl-2.0.txt
 Domain Path:       /languages
-Text Domain:       classicpress-migration-plugin
+Text Domain:       switch-to-classicpress-from-wordpress
 Network:           true
-GitHub Plugin URI: https://github.com/classicpress/classicpress-migration-plugin/
+GitHub Plugin URI: https://github.com/classicpress/switch-to-classicpress-from-wordpress/
 GitHub Branch:     master
 Requires WP:       4.9
 */
 
 	/**
-	 * ClassicPress Migration Plugin.
+	 * Switch to ClassicPress from WordPress.
 	 *
-	 * @package ClassicPress_Migration_Plugin
-	 * @version 1.0.1 - 2018-08-30
+	 * @package Switch_to_ClassicPress_from_WordPress
+	 * @version 1.0.2 - 2018-08-30
 	 * @since   1.0.0 - 2018-08-29
 	 * @todo    ClassicPress syle:
 	 *           - All changes that reflect new direction.
@@ -56,7 +56,7 @@ if ( ! function_exists( 'add_action' ) ) {
  *
  * Start at version 1.0.0 and use SemVer - https://semver.org/
  */
-define( 'CLASSICPRESS_MIGRATION_PLUGIN_VERSION', '1.0.1' );
+define( 'SWITCH_TO_CLASSICPRESS_FROM_WORDPRESS_VERSION', '1.0.2' );
 
 add_filter( 'admin_menu', 'classicpress_remove_gutenberg_demo_menu', 999 );
 add_filter( 'admin_init', 'classicpress_remove_gutenberg_dashboard_widget' );
@@ -68,14 +68,14 @@ add_filter( 'plugin_row_meta', 'classicpress_adds_row_meta_links', 10, 2 );
  * Load Plugin Textdomain.
  */
 function classicpress_load_plugin_textdomain() {
-	load_plugin_textdomain( 'classicpress-migration-plugin', false, basename( dirname( __FILE__ ) ) . '/languages' );
+	load_plugin_textdomain( 'switch-to-classicpress-from-wordpress', false, basename( dirname( __FILE__ ) ) . '/languages' );
 }
 
 /**
  * Load MU-Plugin (dir) Textdomain.
  */
 function classicpress_load_muplugin_textdomain() {
-	load_muplugin_textdomain( 'classicpress-migration-plugin', basename( dirname( __FILE__ ) ) . '/languages' );
+	load_muplugin_textdomain( 'switch-to-classicpress-from-wordpress', basename( dirname( __FILE__ ) ) . '/languages' );
 }
 
 /**
@@ -84,7 +84,7 @@ function classicpress_load_muplugin_textdomain() {
 function classicpress_adds_row_meta_links( $plugin_meta, $plugin_file ) {
 	if ( $plugin_file == plugin_basename( __FILE__ ) )
 		{
-			$plugin_meta[] .= '<a href="https://www.change.org/p/petition-to-wordpress-no-gutenberg-in-wordpress-core/">' . __( 'Sign ClassicPress Petition', 'classicpress-migration-plugin' ) . '</a>';
+			$plugin_meta[] .= '<a href="https://www.change.org/p/petition-to-wordpress-no-gutenberg-in-wordpress-core/">' . __( 'Sign ClassicPress Petition', 'switch-to-classicpress-from-wordpress' ) . '</a>';
 		}
 	return $plugin_meta;
 }
