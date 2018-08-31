edbc3557fa632db8813d7444bde247ff678944ef
Marc Tenax <42794352+marctenax@users.noreply.github.com>
2018-08-31 18:40:48 +0200

    Initial commit

--- switch-to-classicpress-from-wordpress.php
+++ switch-to-classicpress-from-wordpress.php
@@ -3,7 +3,7 @@
 Plugin Name:       Switch to ClassicPress from WordPress
 Plugin URI:        https://github.com/classicpress/classicpress-migration-plugin/
 Description:       Switch to ClassicPress from WordPress and say Goodbye to Gutenberg.
-Version:           1.0.2
+Version:           1.0.0
 Requires at least: 4.9
 Tested up to:      4.9
 Requires PHP:      5.2.4
@@ -25,8 +25,7 @@ Requires WP:       4.9
 	 * ClassicPress Migration Plugin.
 	 *
 	 * @package Switch_to_ClassicPress_from_WordPress
-	 * @version 1.0.2 - 2018-08-30
-	 * @since   1.0.0 - 2018-08-29
+	 * @version 1.0.0
 	 * @todo    ClassicPress syle:
 	 *           - All changes that reflect new direction.
 	 */
@@ -54,17 +53,16 @@ if ( ! function_exists( 'add_action' ) ) {
 }
 
 /**
- * Currently Plugin Version.
+ * Current Plugin Version.
  *
  * Start at version 1.0.0 and use SemVer - https://semver.org/
  */
-define( 'SWITCH_TO_CLASSICPRESS_FROM_WORDPRESS_VERSION', '1.0.2' );
+define( 'SWITCH_TO_CLASSICPRESS_FROM_WORDPRESS_VERSION', '1.0.0' );
 
 add_filter( 'admin_menu', 'classicpress_remove_gutenberg_demo_menu', 999 );
 add_filter( 'admin_init', 'classicpress_remove_gutenberg_dashboard_widget' );
 add_filter( 'plugins_loaded', 'classicpress_load_plugin_textdomain' );
 add_filter( 'plugins_loaded', 'classicpress_load_muplugin_textdomain' );
-add_filter( 'plugin_row_meta', 'classicpress_adds_row_meta_links', 10, 2 );
 
 /**
  * Load Plugin Textdomain.
@@ -78,17 +76,6 @@ function classicpress_load_plugin_textdomain() {
  */
 function classicpress_load_muplugin_textdomain() {
 	load_muplugin_textdomain( 'switch-to-classicpress-from-wordpress', basename( dirname( __FILE__ ) ) . '/languages' );
-}
-
-/**
- * Adds Plugin Row Meta Links.
- */
-function classicpress_adds_row_meta_links( $plugin_meta, $plugin_file ) {
-	if ( $plugin_file == plugin_basename( __FILE__ ) )
-		{
-			$plugin_meta[] .= '<a href="https://www.change.org/p/petition-to-wordpress-no-gutenberg-in-wordpress-core/">' . __( 'Sign ClassicPress Petition', 'switch-to-classicpress-from-wordpress' ) . '</a>';
-		}
-	return $plugin_meta;
 }
 
 /**
