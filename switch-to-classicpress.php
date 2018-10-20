a87b5dea34292b90f733e35197340fd6d38c64f8
Pieter Bos <senlin@users.noreply.github.com>
2018-10-20 17:01:38 +0100

    add link to Switch page on main plugins page
    For some reason this only takes on the root plugin file

--- switch-to-classicpress.php
+++ switch-to-classicpress.php
@@ -56,3 +56,21 @@ require_once dirname( __FILE__ ) . '/lib/admin-page.php';
  * @since 0.0.1
  */
 require_once dirname( __FILE__ ) . '/lib/update.php';
+
+/**
+ * Add plugin action links.
+ *
+ * Add a link to the Switch page on the plugins.php page.
+ *
+ * @since 0.0.1
+ *
+ * @param  array  $links List of existing plugin action links.
+ * @return array         List of modified plugin action links.
+ */
+function classicpress_plugin_action_links( $links ) {
+	$links = array_merge( array(
+		'<a href="' . esc_url( admin_url( 'tools.php?page=switch-to-classicpress' ) ) . '">' . __( 'Switch', 'switch-to-classicpress' ) . '</a>'
+	), $links );
+	return $links;
+}
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'classicpress_plugin_action_links' );
