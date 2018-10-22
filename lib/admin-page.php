7a5aaf741d6c139b078c8c467d48daa8f1373e96
James Nylen <jnylen@gmail.com>
2018-10-21 23:35:17 -0500

    Begin implementing pre-flight checks

--- lib/admin-page.php
+++ lib/admin-page.php
@@ -12,17 +12,72 @@ function classicpress_print_admin_styles() {
 	font-weight: bold;
 	color: #800;
 }
+.cp-migration-ready {
+	font-weight: bold;
+	color: #080;
+}
 .cp-migration-action:hover {
 	color: #f00;
 }
 .cp-migration-info {
 	max-width: 600px;
 }
+table#cp-preflight-checks {
+	margin: 0.8em 0;
+	border-spacing: 0;
+}
+#cp-preflight-checks p {
+	margin: 0;
+}
+#cp-preflight-checks td {
+	padding: 0.5em 0 0.5em 1em;
+	margin: 0;
+}
+#cp-preflight-checks td + td {
+	padding-right: 0;
+}
+#cp-preflight-checks tr + tr td {
+	border-top: 1px solid #ccc;
+}
+.cp-preflight-icon {
+	font-size: 250%;
+	font-weight: bold;
+	border-radius: 0.5em;
+	color: #f1f1f1; /* default wp-admin background */
+	display: block;
+	width: 1em;
+	height: 1em;
+}
+.cp-preflight-icon .dashicons {
+	font-size: 1em;
+	display: block;
+	width: 1em;
+	height: 1em;
+	position: relative;
+	left: -0.025em;
+	top: 0.03em;
+}
+.cp-preflight-icon.cp-pass {
+	background: #080;
+}
+.cp-preflight-icon.cp-fail {
+	background: #800;
+}
 </style>
 <?php
 }
 add_action( 'admin_head-plugins.php', 'classicpress_print_admin_styles' );
 add_action( 'admin_head-tools_page_switch-to-classicpress', 'classicpress_print_admin_styles' );
+
+/**
+ * Removes the WP update nag from the Switch to ClassicPress page.
+ *
+ * @since 0.0.1
+ */
+function classicpress_remove_update_nag() {
+	remove_action( 'admin_notices', 'update_nag', 3 );
+}
+add_action( 'admin_head-tools_page_switch-to-classicpress', 'classicpress_remove_update_nag' );
 
 /**
  * Registers the plugin's admin page under the Tools menu.
@@ -49,8 +104,38 @@ function classicpress_show_admin_page() {
 ?>
 <div class="wrap">
 	<h1><?php esc_html_e( 'Switch to ClassicPress', 'switch-to-classicpress' ); ?></h1>
+<?php
+	$preflight_checks_ok = classicpress_check_can_migrate();
 
-	<?php if ( function_exists( 'classicpress_version' ) ) { ?>
+	if ( $preflight_checks_ok ) {
+		classicpress_show_migration_controls();
+	} else {
+		// TODO: implement this function
+		classicpress_show_migration_blocked_info();
+	}
+
+	// TODO: Add a message about how to contact ClassicPress
+?>
+</div><!-- .wrap -->
+<?php
+}
+
+/**
+ * Determines whether this WP install can be migrated to ClassicPress.
+ *
+ * Also outputs messages relevant to the checks performed.
+ *
+ * @since 0.0.1
+ *
+ * @return bool Whether to show the controls to proceed with the migration.
+ */
+function classicpress_check_can_migrate() {
+	// First: Run a series of checks for conditions that are inherent to this
+	// WordPress install and this user session.
+
+	// Check: Are we already on ClassicPress?
+	if ( function_exists( 'classicpress_version' ) ) {
+?>
 		<div class="notice notice-success">
 			<p>
 				<?php esc_html_e(
@@ -69,11 +154,13 @@ function classicpress_show_admin_page() {
 				); ?>
 			</p>
 		</div>
-</div><!-- .wrap -->
-		<?php return;
-	} ?>
+<?php
+		return false;
+	}
 
-	<?php if ( defined( 'WPCOMSH_VERSION' ) && WPCOMSH_VERSION ) { ?>
+	// Check: Are we running on WordPress.com?
+	if ( defined( 'WPCOMSH_VERSION' ) && WPCOMSH_VERSION ) {
+?>
 		<div class="notice notice-error">
 			<p>
 				<?php esc_html_e(
@@ -88,11 +175,13 @@ function classicpress_show_admin_page() {
 				); ?>
 			</p>
 		</div>
-</div><!-- .wrap -->
-		<?php return;
-	} ?>
+<?php
+		return false;
+	}
 
-	<?php if ( ! current_user_can( 'update_core' ) ) { ?>
+	// Check: Does the current user have permission to update core?
+	if ( ! current_user_can( 'update_core' ) ) {
+?>
 		<div class="notice notice-error">
 			<p>
 				<?php esc_html_e(
@@ -107,19 +196,154 @@ function classicpress_show_admin_page() {
 				); ?>
 			</p>
 		</div>
-</div><!-- .wrap -->
-		<?php return;
-	} ?>
-
 <?php
-	// TODO: pre-flight checks:
-	// - PHP >= 5.6
-	// - wp_http_supports( array( 'ssl' ) )
-	// - FS_METHOD === 'direct'
-	// - Not a multisite install
-	// - Any others?
-?>
+		return false;
+	}
 
+	// Check: Is this a multisite install?
+	if ( is_multisite() ) {
+?>
+		<div class="notice notice-error">
+			<p>
+				<?php esc_html_e(
+					"Sorry, this plugin doesn't support multisite installations.",
+					'switch-to-classicpress'
+				); ?>
+			</p>
+		</div>
+<?php
+		return false;
+	}
+
+	// The first round of checks has passed.  Now, run a second round related
+	// to conditions that the user (or at least the hosting provider) has
+	// control over, and display the results in a table.
+	
+	$preflight_checks = array();
+	$icon_preflight_pass = (
+		'<div class="cp-preflight-icon cp-pass">'
+			. '<div class="dashicons dashicons-yes"></div>'
+		. '</div>'
+	);
+	$icon_preflight_fail = (
+		'<div class="cp-preflight-icon cp-fail">'
+			. '<div class="dashicons dashicons-no"></div>'
+		. '</div>'
+	);
+	echo '<table id="cp-preflight-checks">' . "\n";
+
+	// Check: Supported WP version
+	// More versions can be added if they pass the plugin's automated tests.
+	global $wp_version;
+	$wp_version_min = '4.9.0';
+	$wp_version_max = '4.9.8';
+	if (
+		version_compare( $wp_version, $wp_version_min, 'lt' ) ||
+		version_compare( $wp_version, $wp_version_max, 'gt' )
+	) {
+		$preflight_checks['wp_version'] = false;
+		echo "<tr>\n<td>$icon_preflight_fail</td>\n<td>\n";
+	} else {
+		$preflight_checks['wp_version'] = true;
+		echo "<tr>\n<td>$icon_preflight_pass</td>\n<td>\n";
+	}
+	echo "<p>\n";
+	printf( __(
+		/* translators: 1: minimum supported WordPress version, 2: maximum supported WordPress version */
+		'This plugin supports WordPress versions <strong>%1$s</strong> to <strong>%2$s</strong>.',
+		'switch-to-classicpress'
+	), $wp_version_min, $wp_version_max );
+	echo "<br>\n";
+	printf( __(
+		/* translators: current WordPress version */
+		'You are running WordPress version <strong>%s</strong>.',
+		'switch-to-classicpress'
+	), $wp_version );
+	echo "\n</p>\n";
+	// TODO: Add instructions if WP too old.
+	echo "</td></tr>\n";
+
+	// Check: Supported PHP version
+	$php_version_min = '5.6';
+	if ( version_compare( PHP_VERSION, $php_version_min, 'lt' ) ) {
+		$preflight_checks['php_version'] = false;
+		echo "<tr>\n<td>$icon_preflight_fail</td>\n<td>\n";
+	} else {
+		$preflight_checks['php_version'] = true;
+		echo "<tr>\n<td>$icon_preflight_pass</td>\n<td>\n";
+	}
+	echo "<p>\n";
+	printf( __(
+		/* translators: minimum supported PHP version */
+		'ClassicPress supports PHP versions <strong>%1$s</strong> and <strong>newer</strong>.',
+		'switch-to-classicpress'
+	), $php_version_min );
+	echo "<br>\n";
+	printf( __(
+		/* translators: current PHP version */
+		'You are using PHP version <strong>%s</strong>.',
+		'switch-to-classicpress'
+	), PHP_VERSION );
+	echo "\n</p>\n";
+	// TODO: Add instructions if PHP too old.
+	echo "</td></tr>\n";
+
+	// Check: Support for outgoing HTTPS requests
+	if ( ! wp_http_supports( array( 'ssl' ) ) ) {
+		$preflight_checks['wp_http_supports_ssl'] = false;
+		echo "<tr>\n<td>$icon_preflight_fail</td>\n<td>\n";
+		echo "<p>\n";
+		_e(
+			'This site <strong>does not</strong> support making outgoing connections securely using SSL.',
+			'switch-to-classicpress'
+		);
+	} else {
+		$preflight_checks['wp_http_supports_ssl'] = true;
+		echo "<tr>\n<td>$icon_preflight_pass</td>\n<td>\n";
+		echo "<p>\n";
+		_e(
+			'This site supports making outgoing connections securely using SSL.',
+			'switch-to-classicpress'
+		);
+	}
+	echo "\n<br>\n";
+	_e(
+		'ClassicPress only supports communicating with the ClassicPress.net API over SSL.',
+		'switch-to-classicpress'
+	);
+	// TODO: Add instructions if SSL not supported.
+	echo "\n</p>\n";
+
+	// TODO: Any other checks needed?
+
+	echo "</table>\n";
+
+	if (
+		$preflight_checks['wp_version'] &&
+		$preflight_checks['php_version'] &&
+		$preflight_checks['wp_http_supports_ssl']
+	) {
+		update_option( 'classicpress_preflight_checks', $preflight_checks, false );
+		return true;
+	} else {
+		delete_option( 'classicpress_preflight_checks' );
+		return false;
+	}
+}
+
+/**
+ * Shows the controls and information needed to migrate to ClassicPress.
+ *
+ * NOTE: ONLY CALL THIS FUNCTION IF ALL PRE-FLIGHT CHECKS HAVE PASSED!
+ * Otherwise you *will* end up with a broken site!
+ *
+ * @since 0.0.1
+ */
+function classicpress_show_migration_controls() {
+?>
+	<h2 class="cp-migration-info cp-migration-ready">
+		<?php _e( "It looks like you're ready to switch to ClassicPress!", 'switch-to-classicpress' ); ?>
+	</h2>
 	<p class="cp-migration-info">
 		<?php _e( 'First things first, just in case something does not go as planned, <strong class="cp-emphasis">please make a backup of your site files and database</strong>.', 'switch-to-classicpress' ); ?>
 	</p>
@@ -153,9 +377,7 @@ function classicpress_show_admin_page() {
 		<?php _e( 'Once the process has completed, you will see the about page of ClassicPress where you can read more information about the project.', 'switch-to-classicpress' ); ?>
 	</p>
 	<p class="cp-migration-info">
-		<?php _e( 'We thank you for switching WordPress to ClassicPress! The business-focused CMS. Powerful. Versatile. Predictable.', 'switch-to-classicpress' ); ?>
+		<?php _e( 'We thank you for switching WordPress to ClassicPress!<br>The business-focused CMS. Powerful. Versatile. Predictable.', 'switch-to-classicpress' ); ?>
 	</p>
-
-</div><!-- .wrap -->
 <?php
 }
