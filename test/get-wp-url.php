ff7eb9bb3c24927b265fef02947853111cf34d31
James Nylen <jnylen@gmail.com>
2018-11-21 00:08:54 -0500

    Support spinning up a new multisite
    Also improve / clean up site URLs and URL determination.

---
+++ test/get-wp-url.php
@@ -0,0 +1,18 @@
+<?php
+
+$lando_info = json_decode( file_get_contents( 'php://stdin' ), true );
+
+if ( empty( $lando_info ) || empty( $argv[1] ) ) {
+	die( "Usage: lando info | $argv[0] url-pattern\n" );
+}
+$match = $argv[1];
+
+foreach ( $lando_info['appserver']['urls'] as $url ) {
+	if ( strstr( $url, $match ) !== false ) {
+		print "$url\n";
+		exit( 0 );
+	}
+}
+
+fwrite( STDERR, "ERROR: No matching URL found!\n" );
+exit( 1 );
