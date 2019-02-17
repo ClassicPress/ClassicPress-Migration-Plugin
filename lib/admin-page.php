8ca8d42390b0176e43d1118cb44f041bdd64ee56
invisnet <classicpress@invis.net>
2019-02-17 01:38:21 +0000

    A fix for #344

--- lib/admin-page.php
+++ lib/admin-page.php
@@ -449,6 +449,36 @@ function classicpress_check_can_migrate() {
 	echo "\n</p>\n";
 	echo "</td></tr>\n";
 
+	// Check: Conflicting files
+	$conflicting_files = classicpress_check_conflicting_files();
+	if ( $conflicting_files === false || ! empty( $conflicting_files ) ) {
+		$preflight_checks['conflicting_files'] = false;
+		echo "<tr>\n<td>$icon_preflight_fail</td>\n<td>\n";
+	} else {
+		$preflight_checks['conflicting_files'] = true;
+		echo "<tr>\n<td>$icon_preflight_pass</td>\n<td>\n";
+	}
+	if ( empty( $conflicting_files ) ) {
+		_e(
+			'No conflicting files were found.',
+			'switch-to-classicpress'
+		);
+	} else {
+		echo '<strong class="cp-emphasis">';
+		_e(
+			'Conflicting files detected.',
+			'switch-to-classicpress'
+		);
+		echo "</strong>\n<br>\n";
+		_e(
+			'If you have JavaScript enabled, you can see a list of conflicting files <strong>in your browser console</strong>.',
+			'switch-to-classicpress'
+		);
+		echo "\n<script>console.log( 'modified core files:', ";
+		echo wp_json_encode( $conflicting_files );
+		echo ' );</script>';
+	}
+
 	// Check: Core files checksums
 	$modified_files = classicpress_check_core_files();
 	if ( $modified_files === false || ! empty( $modified_files ) ) {
@@ -524,7 +554,8 @@ function classicpress_check_can_migrate() {
 	if (
 		$preflight_checks['wp_version'] &&
 		$preflight_checks['php_version'] &&
-		$preflight_checks['wp_http_supports_ssl']
+		$preflight_checks['wp_http_supports_ssl'] &&
+		$preflight_checks['conflicting_files']
 	) {
 		update_option( 'classicpress_preflight_checks', $preflight_checks, false );
 		return true;
