6eb6216edc6541a6c35fa5a97f1e760ecc37f3da
James Nylen <jnylen@gmail.com>
2019-09-15 23:06:11 -0500

    Document the workaround

--- test/get-wp-url.php
+++ test/get-wp-url.php
@@ -3,7 +3,7 @@
 $lando_info = json_decode( file_get_contents( 'php://stdin' ), true );
 
 if ( empty( $lando_info ) || empty( $argv[1] ) ) {
-	die( "Usage: lando info | $argv[0] url-pattern\n" );
+	die( "Usage: lando info --format=json | $argv[0] url-pattern\n" );
 }
 $match = $argv[1];
 
